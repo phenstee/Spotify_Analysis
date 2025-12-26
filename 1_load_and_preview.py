8d11c137ef75e4e39cb1cd268c4494b1c11a8114
phenstee <yifeic2007@gmail.com>
2025-12-26 13:50:14 -0500

    load and preview data

--- 1_load_and_preview.py
+++ 1_load_and_preview.py
@@ -10,3 +10,17 @@ json_files = list(raw_folder.glob("*.json"))
 print("Found files")
 for file in json_files:
     print(f"- {file.name}")
+
+
+# Load and combine them into one df
+dfs = []
+for file in json_files:
+    df = pd.read_json(file)
+    dfs.append(df)
+
+plays_raw = pd.concat(dfs, ignore_index=True)
+
+print("\nrows:", len(plays_raw))
+print("Columns:", list(plays_raw.columns))
+print("\nFirst 5 rows: ")
+print(plays_raw.head())
